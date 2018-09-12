455adb98808bd604a636df5d064f35053da2dac2
Mehmood <halcyoona@gmail.com>
2018-09-12 12:03:55 +0500

    g

--- 01-week-lec/pointer.cpp
+++ 01-week-lec/pointer.cpp
@@ -3,30 +3,34 @@
 using namespace std;
 
 void simplePointer(){
-	int *integerPointer = new int;
+	// int *integerPointer = new int;
+	// int integerValue = 0;
+	
+	// cout<<"Enter a Integer: ";
+	// cin>>*integerPointer;
+	
+	// integerValue = *integerPointer;
+
+
+	// cout<<integerPointer<<endl;
+	// cout<<*integerPointer<<endl;
+	// cout<<&integerPointer<<endl;
+	// cout<<&*integerPointer<<endl;
+	// cout<<*&integerPointer<<endl;
+	// cout<<integerValue<<endl;
+	// cout<<&integerValue<<endl;
+	// // cout<<&*integerValue<<endl; can not be done because it is not a pointer  
+	// cout<<*&integerValue<<endl;
+
+
+    cout<<"-------CHAR------ "<<endl;
+
 	char *charPointer;
-	int integerValue = 0;
 	char charValue;
-	cout<<"Enter a Integer: ";
-	cin>>*integerPointer;
 	cout<<"Enter a character: ";
 	cin>>charValue;
-	integerValue = *integerPointer;
 	charPointer = &charValue;
 
-
-	cout<<integerPointer<<endl;
-	cout<<*integerPointer<<endl;
-	cout<<&integerPointer<<endl;
-	cout<<&*integerPointer<<endl;
-	cout<<*&integerPointer<<endl;
-	cout<<integerValue<<endl;
-	cout<<&integerValue<<endl;
-	// cout<<&*integerValue<<endl; can not be done because it is not a pointer  
-	cout<<*&integerValue<<endl;
-	
-
-	cout<<"-------CHAR------ "<<endl;
 	cout<<charPointer<<endl;
 	cout<<*charPointer<<endl;
 	cout<<&charPointer<<endl;
