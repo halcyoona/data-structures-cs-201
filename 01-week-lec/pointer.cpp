b1afa1e473f3858aa07c4b14af4cc09a0cbfcdba
halcyoona <halcyoona@gmail.com>
2018-09-20 10:46:16 +0500

    g

--- 01-week-lec/pointer.cpp
+++ 01-week-lec/pointer.cpp
@@ -22,24 +22,47 @@ void simplePointer(){
 	// // cout<<&*integerValue<<endl; can not be done because it is not a pointer  
 	// cout<<*&integerValue<<endl;
 
+	float *floatPointer = new float;
+	float floatValue = 0;
+	
+	cout<<"Enter a float: ";
+	cin>>*floatPointer;
+	
+	floatValue = *floatPointer;
 
-    cout<<"-------CHAR------ "<<endl;
 
-	char *charPointer;
-	char charValue;
-	cout<<"Enter a character: ";
-	cin>>charValue;
-	charPointer = &charValue;
+	cout<<floatPointer<<endl;
+	cout<<*floatPointer<<endl;
+	cout<<&floatPointer<<endl;
+	cout<<&*floatPointer<<endl;
+	cout<<*&floatPointer<<endl;
+	cout<<floatValue<<endl;
+	cout<<&floatValue<<endl;
+	// cout<<&*integerValue<<endl; can not be done because it is not a pointer  
+	cout<<*&floatValue<<endl;
 
-	cout<<charPointer<<endl;
-	cout<<*charPointer<<endl;
-	cout<<&charPointer<<endl;
-	cout<<*&charPointer<<endl;
-	cout<<&*charPointer<<endl;
-	cout<<charValue<<endl;
-	cout<<&charValue<<endl;
-	// cout<<&*charValue<<endl; can not be done because it is not a pointer
-	cout<<*&charValue<<endl;
+
+
+
+
+ //    cout<<"-------CHAR------ "<<endl;
+
+	// char *charPointer;
+	// char charValue;
+	// cout<<"Enter a character: ";
+	// cin>>charValue;
+	// charPointer = &charValue;
+
+	// cout<<charPointer<<endl;
+	// cout<<*charPointer<<endl;
+	// cout<<&charPointer<<endl;
+	// cout<<*&charPointer<<endl;
+	// cout<<&*charPointer<<endl;
+	// cout<<charValue<<endl;
+	// cout<<&charValue<<endl;
+	// // cout<<&*charValue<<endl; can not be done because it is not a pointer
+	// cout<<*&charValue<<endl;
+
 }
 
 
