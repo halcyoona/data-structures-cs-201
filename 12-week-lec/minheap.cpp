bb797e0836319d0649996d19c5810f914970c70d
halcyoona <halcyoona@gmail.com>
2018-12-16 11:47:28 +0500

    g

--- 12-week-lec/minheap.cpp
+++ 12-week-lec/minheap.cpp
@@ -16,6 +16,8 @@ class heap
 		int height;
 		int localHeight;
 		int countNode;
+		Node *searchTemp;
+		Node *delTemp;
 	public:
 		Node *root;
 		int number;
@@ -26,6 +28,9 @@ class heap
 		void inOrder(Node *temp);
 		void insert(Node *temp);
 		int power(int height, int n);
+		void del(Node *temp);
+		void search(Node *temp);
+		void lowerBubble(Node *temp);
 };
 
 
@@ -46,6 +51,10 @@ heap::~heap()
 
 void heap::delAll(Node *temp)
 {
+	if (root == NULL)
+	{
+		return;
+	}
 	if (temp->left!=NULL)
 		delAll(temp->left);
 	if (temp->right!=NULL)
@@ -55,6 +64,11 @@ void heap::delAll(Node *temp)
 
 void heap::inOrder(Node *temp)
 {
+	if (temp == NULL)
+	{
+		cout<<"tree is empty"<<endl;
+		return;
+	}
 	if (temp->left != NULL)
 		inOrder(temp->left);
 	cout<<temp->data<<" ";
@@ -197,6 +211,146 @@ int heap::power(int num, int n)
 
 
 
+
+void heap::del(Node *temp)
+{
+	if (temp == root)
+	{
+		flag = 0;
+		delTemp = NULL;
+	}
+
+	if (root == NULL)
+	{
+		cout<<"tree is empty"<<endl;
+		return;
+	}
+	
+	if (countNode == 1)
+	{
+		Node *temp2 = root;
+		delete temp2;
+		root = NULL;
+		countNode -= 1;
+		height -= 1;
+		return;
+	}
+
+	if ((power(2,height)) == countNode) 
+	{
+		// cout<<"2"<<endl;
+		while(temp->left->left != NULL)
+			temp = temp->left;
+		search(root);
+		searchTemp->data = temp->left->data;
+		delete temp->left;
+		temp->left = NULL;
+		height -= 1;
+		countNode -=1;
+		flag = 1;
+		return;
+	}
+
+	if (((power(2,height+1))-1) == countNode)	
+	{
+		// cout<<"3"<<endl;
+		while(temp->right->right != NULL)
+			temp = temp->right;
+		search(root);
+		// cout<<"search: "<<searchTemp->data<<endl;
+		searchTemp->data = temp->right->data;
+		// cout<<"search: "<<searchTemp->data<<endl;
+		delete temp->right;
+		temp->right = NULL;
+		countNode -=1;
+		flag = 1;
+		return;
+	}
+
+	if (height-localHeight == 1)
+	{
+		// cout<<"4"<<endl;
+		if (temp->left != NULL && temp->right != NULL)
+		{
+			delTemp = temp;
+		}
+		if (temp->left != NULL && temp->right == NULL)
+		{
+			search(root);
+			searchTemp->data = temp->left->data;
+			delete temp->left;
+			temp->left = NULL;
+			countNode -=1;
+			flag = 1;
+			return;
+
+		}
+	}
+
+	if (temp->left != NULL && flag == 0)
+	{	
+		// cout<<"5"<<endl;
+		localHeight += 1;
+		del(temp->left);
+		localHeight -= 1;
+	}
+
+	if (temp->right != NULL && flag == 0)
+	{
+		// cout<<"6"<<endl;
+		localHeight += 1;
+		del(temp->right);
+		localHeight -= 1;
+	}
+	if (temp == root)
+	{
+		if (delTemp != NULL)
+		{
+			searchTemp->data = delTemp->right->data;
+			delete delTemp->right;
+			delTemp->right = NULL;
+		}
+	}
+	
+	return;
+}
+
+
+void heap::search(Node *temp)
+{
+	lowerBubble(temp);
+	return;
+}
+
+void heap:: lowerBubble(Node *temp)
+{
+	if (temp->left == NULL)
+	{
+		searchTemp = temp;
+		return;
+	}
+	if (temp->left != NULL && temp->right == NULL)
+	{
+		temp->data = temp->left->data;
+		lowerBubble(temp->left);
+	}
+	if (temp->left != NULL && temp->right != NULL)
+	{
+		if (temp->left->data < temp->right->data)
+		{
+			temp->data = temp->left->data;
+			lowerBubble(temp->left);
+		}
+		else
+		{
+			temp->data = temp->right->data;
+			lowerBubble(temp->right);
+		}
+	}
+}
+
+
+
 int main()
 {
 
@@ -206,22 +360,48 @@ int main()
 	obj.insert(obj.root);
 	obj.number = 30;
 	obj.insert(obj.root);
-	obj.number = 42;
-	obj.insert(obj.root);
+	// obj.number = 42;
+	// obj.insert(obj.root);
+	// // obj.inOrder(obj.root);
+	// // cout<<endl;
+	// obj.number = 10;
+	// obj.insert(obj.root);
+	// obj.number = 67;
+	// obj.insert(obj.root);
+	// obj.number = 71;
+	// obj.insert(obj.root);
+	// obj.number = 6;
+	// obj.insert(obj.root);
+	// obj.number = 9;
+	// obj.insert(obj.root);
+	obj.inOrder(obj.root);
+	cout<<endl;
+	// obj.del(obj.root);
 	// obj.inOrder(obj.root);
 	// cout<<endl;
-	obj.number = 10;
-	obj.insert(obj.root);
-	obj.number = 67;
-	obj.insert(obj.root);
-	obj.number = 71;
-	obj.insert(obj.root);
-	obj.number = 6;
-	obj.insert(obj.root);
-	obj.number = 9;
-	obj.insert(obj.root);
-	// obj.number = 11;
+	// obj.del(obj.root);
+	// obj.inOrder(obj.root);
+	// cout<<endl;
+	// obj.del(obj.root);
+	// obj.inOrder(obj.root);
+	// cout<<endl;
+	// obj.del(obj.root);
+	// obj.inOrder(obj.root);
+	// cout<<endl;
+	// obj.del(obj.root);
+	// obj.inOrder(obj.root);
+	// cout<<endl;
+	obj.del(obj.root);
+	obj.inOrder(obj.root);
+	cout<<endl;
+	obj.del(obj.root);
+	obj.inOrder(obj.root);
+	cout<<endl;
+
 	// obj.insert(obj.root);
+	// obj.number = 2;
+	obj.number = 11;
+	obj.insert(obj.root);
 	// obj.number = 25;
 	// obj.insert(obj.root);
 	// obj.number = 36;
